fd0deac7340a2557367fb162e28ba1546ba5f518
Lobo808 <jlsun808@gmail.com>
2025-03-31 21:46:39 -0400

    Add download button

--- src/pages/rpg/RpgPage.tsx
+++ src/pages/rpg/RpgPage.tsx
@@ -1,20 +1,28 @@
-import { Text } from "@mantine/core";
+import { Button, Text } from "@mantine/core";
 
 import { DataContextProvider, useDataContext } from "./context";
 import { LoadDataModal } from "./load-data-modal";
+import { PageOverlay } from "./page-overlay";
 
 const PlaceHolderComponent = () => {
-  const { data } = useDataContext();
-  return (
-    <Text size="xl" mb="md">
-      {data ? JSON.stringify(data, null, 2) : ""}
-    </Text>
-  );
+    const { data, setData } = useDataContext();
+
+    return (
+        <>
+            <Text size="xl" mb="md">
+                {data ? JSON.stringify(data, null, 2) : ""}
+            </Text>
+            <Button onClick={() => setData({ updated: "updated value" })}>
+                Update data
+            </Button>
+        </>
+    );
 };
 
 export const RpgPage = () => (
-  <DataContextProvider>
-    <LoadDataModal />
-    <PlaceHolderComponent />
-  </DataContextProvider>
+    <DataContextProvider>
+        <LoadDataModal />
+        <PageOverlay />
+        <PlaceHolderComponent />
+    </DataContextProvider>
 );
